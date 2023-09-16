8da2d277367a8a8747dfd4b2695b0e563078a557
Sinclair <tpyzlxy@gmail.com>
2023-09-16 12:15:54 +0800

    update env

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,8 +1,9 @@
 // 配置文件
 
-//const host = '/' + basePath;
-let host = 'http://127.0.0.1:8001/system/api';
-//host = '/system/api';
+let host = '/system/api';
+if (process.env.NODE_ENV === 'development') {
+  host = 'http://127.0.0.1:8001/system/api';
+}
 
 const config = {
   baseUrl: host,
