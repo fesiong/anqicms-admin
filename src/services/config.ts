46ecc4d8b17308436fe057d2183fa7afd844ce9f
Sinclair <tpyzlxy@gmail.com>
2026-01-02 18:25:22 +0800

    fix

--- src/services/config.ts
+++ src/services/config.ts
@@ -16,7 +16,6 @@ if (typeof window !== 'undefined' && window.APP_CONFIG) {
 if (process.env.NODE_ENV === 'development') {
   host = 'http://127.0.0.1:8001/system/api';
 }
-host = 'https://www.anqicms.com/system/api';
 
 const config = {
   baseUrl: host,
