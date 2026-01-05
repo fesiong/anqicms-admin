d8576038a0e6a36703e081e489e6743a2f41e028
Sinclair <tpyzlxy@gmail.com>
2026-01-05 22:18:07 +0800

    add h2,h3 to AIEditor's float menu

--- src/components/newAiEditor/index.tsx
+++ src/components/newAiEditor/index.tsx
@@ -13,7 +13,7 @@ import {
   useRef,
   useState,
 } from 'react';
-import Attachment from '../attachment/dialog';
+import AttachmentSelect from '../attachment';
 import './index.less';
 import MaterialSelect, { MaterialElement } from './material';
 
@@ -32,6 +32,7 @@ const NewAiEditor: React.FC<NewAiEditorProps> = forwardRef((props, ref) => {
   const [curEditor, setCurEditor] = useState<AiEditor | null>(null);
   const [videoUrl, setVideoUrl] = useState<string>('');
   const [materialVisible, setMaterialVisible] = useState(false);
+  const [attachVisible, setAttachVisible] = useState(false);
   const intl = useIntl();
   function setInnerContent(content: string) {
     aiEditorRef.current?.setContent(content);
@@ -41,67 +42,68 @@ const NewAiEditor: React.FC<NewAiEditorProps> = forwardRef((props, ref) => {
     setInnerContent: setInnerContent,
   }));
 
+  const getImagehtml = (item: any) => {
+    if (
+      item.is_image ||
+      item.file_location.indexOf('.webp') !== -1 ||
+      item.file_location.indexOf('.bmp') !== -1 ||
+      item.file_location.indexOf('.png') !== -1 ||
+      item.file_location.indexOf('.gif') !== -1 ||
+      item.file_location.indexOf('.jpg') !== -1 ||
+      item.file_location.indexOf('.jpeg') !== -1 ||
+      item.file_location.indexOf('.svg') !== -1
+    ) {
+      // img
+      return `<img src="${item.logo}" alt="${item.file_name}" title="${item.file_name}" />`;
+      //return `![${item.file_name}](${item.logo})`;
+    } else if (
+      item.is_image === 2 ||
+      item.file_location.indexOf('.mp4') !== -1 ||
+      item.file_location.indexOf('.ogg') !== -1 ||
+      item.file_location.indexOf('.webm') !== -1
+    ) {
+      return `<video controls="controls" controlslist="nodownload" poster=""><source src="${
+        item.logo
+      }" type="video/${item.file_location.substr(
+        item.file_location.lastIndexOf('.') + 1,
+      )}">Your browser does not support video tags.</video>`;
+    } else if (
+      item.file_location.indexOf('.mp3') !== -1 ||
+      item.file_location.indexOf('.wav') !== -1
+    ) {
+      return `<audio src="${item.logo}" controls="controls"><source src="${
+        item.logo
+      }" type="audio/${item.file_location.substr(
+        item.file_location.lastIndexOf('.') + 1,
+      )}">Your browser does not support audio tags.</audio>`;
+    } else if (item.is_image === 3) {
+      return `<iframe src="${item.file_location}" title="${item.file_name}" width="100%" height="480px" frameborder="0" allowfullscreen="true"></iframe>`;
+    } else {
+      return `<a href="${item.logo}" target="_blank" title="${item.file_name}">${item.file_name}</a>`;
+    }
+  };
   function attachPlugin(editor: AiEditor) {
-    const setImage = (item: any) => {
-      if (
-        item.is_image ||
-        item.file_location.indexOf('.webp') !== -1 ||
-        item.file_location.indexOf('.bmp') !== -1 ||
-        item.file_location.indexOf('.png') !== -1 ||
-        item.file_location.indexOf('.gif') !== -1 ||
-        item.file_location.indexOf('.jpg') !== -1 ||
-        item.file_location.indexOf('.jpeg') !== -1 ||
-        item.file_location.indexOf('.svg') !== -1
-      ) {
-        // img
-        return `<img src="${item.logo}" alt="${item.file_name}" title="${item.file_name}" />`;
-        //return `![${item.file_name}](${item.logo})`;
-      } else if (
-        item.is_image === 2 ||
-        item.file_location.indexOf('.mp4') !== -1 ||
-        item.file_location.indexOf('.ogg') !== -1 ||
-        item.file_location.indexOf('.webm') !== -1
-      ) {
-        return `<video controls="controls" controlslist="nodownload" poster=""><source src="${
-          item.logo
-        }" type="video/${item.file_location.substr(
-          item.file_location.lastIndexOf('.') + 1,
-        )}">Your browser does not support video tags.</video>`;
-      } else if (
-        item.file_location.indexOf('.mp3') !== -1 ||
-        item.file_location.indexOf('.wav') !== -1
-      ) {
-        return `<audio src="${item.logo}" controls="controls"><source src="${
-          item.logo
-        }" type="audio/${item.file_location.substr(
-          item.file_location.lastIndexOf('.') + 1,
-        )}">Your browser does not support audio tags.</audio>`;
-      } else if (item.is_image === 3) {
-        return `<iframe src="${item.file_location}" title="${item.file_name}" width="100%" height="480px" frameborder="0" allowfullscreen="true"></iframe>`;
-      } else {
-        return `<a href="${item.logo}" target="_blank" title="${item.file_name}">${item.file_name}</a>`;
-      }
-    };
-
-    Attachment.show(true, intl).then((res: any) => {
-      let addon = [];
-      for (let i in res) {
-        if (res.hasOwnProperty(i)) {
-          addon.push(setImage(res[i]));
-        }
-      }
-      if (!editor.isFocused()) {
-        editor.focus();
-      }
-      editor.insert(addon.join('\n'));
-      setVideoVisible(false);
-    });
+    setCurEditor(editor);
+    setAttachVisible(true);
   }
 
   const handleSelectVideo = () => {
     if (curEditor) {
       attachPlugin(curEditor);
     }
+  };
+
+  const handleSelectAttachment = (rows: any[]) => {
+    let addon = [];
+    for (let i = 0; i < rows.length; i++) {
+      addon.push(getImagehtml(rows[i]));
+    }
+    if (!curEditor?.isFocused()) {
+      curEditor?.focus();
+    }
+    curEditor?.insert(addon.join('\n'));
+    setAttachVisible(false);
+    setVideoVisible(false);
   };
 
   const showVideo = (editor: AiEditor) => {
@@ -292,6 +294,55 @@ const NewAiEditor: React.FC<NewAiEditorProps> = forwardRef((props, ref) => {
             return handleUpload(file);
           },
         },
+        textSelectionBubbleMenu: {
+          enable: true,
+          items: [
+            'ai',
+            {
+              id: 'h2',
+              title: 'h2',
+              icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M4 4V11H11V4H13V20H11V13H4V20H2V4H4ZM18.5 8C20.5711 8 22.25 9.67893 22.25 11.75C22.25 12.6074 21.9623 13.3976 21.4781 14.0292L21.3302 14.2102L18.0343 18H22V20H15L14.9993 18.444L19.8207 12.8981C20.0881 12.5908 20.25 12.1893 20.25 11.75C20.25 10.7835 19.4665 10 18.5 10C17.5818 10 16.8288 10.7071 16.7558 11.6065L16.75 11.75H14.75C14.75 9.67893 16.4289 8 18.5 8Z"></path></svg>',
+              onClick: ({ innerEditor }) => {
+                if (innerEditor.isActive('heading', { level: 2 })) {
+                  return innerEditor.chain().setParagraph().focus().run();
+                }
+                return innerEditor
+                  .chain()
+                  .setHeading({ level: 2 as any })
+                  .focus()
+                  .run();
+              },
+              isActive: (innerEditor) => {
+                return innerEditor.isActive('heading', { level: 2 });
+              },
+            },
+            {
+              id: 'h3',
+              title: 'h3',
+              icon: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M22 8L21.9984 10L19.4934 12.883C21.0823 13.3184 22.25 14.7728 22.25 16.5C22.25 18.5711 20.5711 20.25 18.5 20.25C16.674 20.25 15.1528 18.9449 14.8184 17.2166L16.7821 16.8352C16.9384 17.6413 17.6481 18.25 18.5 18.25C19.4665 18.25 20.25 17.4665 20.25 16.5C20.25 15.5335 19.4665 14.75 18.5 14.75C18.214 14.75 17.944 14.8186 17.7056 14.9403L16.3992 13.3932L19.3484 10H15V8H22ZM4 4V11H11V4H13V20H11V13H4V20H2V4H4Z"></path></svg>',
+              onClick: ({ innerEditor }) => {
+                if (innerEditor.isActive('heading', { level: 3 })) {
+                  return innerEditor.chain().setParagraph().focus().run();
+                }
+                return innerEditor
+                  .chain()
+                  .setHeading({ level: 3 as any })
+                  .focus()
+                  .run();
+              },
+              isActive: (innerEditor) => {
+                return innerEditor.isActive('heading', { level: 3 });
+              },
+            },
+            'Bold',
+            'Italic',
+            'Underline',
+            'Strike',
+            'link',
+            'heading',
+            'code',
+          ],
+        },
       });
 
       aiEditorRef.current = aiEditor;
@@ -395,6 +446,13 @@ const NewAiEditor: React.FC<NewAiEditorProps> = forwardRef((props, ref) => {
           onSelect={handleSelectMaterial}
         />
       )}
+      <AttachmentSelect
+        open={attachVisible}
+        onCancel={() => setAttachVisible(false)}
+        onSelect={handleSelectAttachment}
+        multiple={true}
+        manual={true}
+      />
     </div>
   );
 });
